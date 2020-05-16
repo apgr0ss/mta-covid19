f06404d45596cbdbd3e124ebb4434df06d8b0dea
apgr0ss <andrew.gross.bl@gmail.com>
2020-05-16 15:34:45 -0500

    Added new features to Station class. Everything is now prepared for me to pull in Census data.

--- scripts/station_to_census_map.py
+++ scripts/station_to_census_map.py
@@ -14,6 +14,12 @@ pull in relevant demographic data
 NOTE: Filtered out stations that did not achieve a 68 or higher Levenshtein ratio
 """
 
+NYC_COUNTY_FIPS = {'Manhattan':'061',
+                   'Brooklyn':'047',
+                   'Queens':'081',
+                   'Bronx':'005',
+                   'Staten Island':'085'}
+
 class Station:
     """
     Represent each station as a class
@@ -32,31 +38,48 @@ class Station:
     def __init__(self,short_name):
         self.short_name = short_name
         self.long_name  = station_to_shp_name_dict[short_name]
-
+        self.tract = ''
+        self.borough_name = ''
         # Call methods
         self.find_point()
 
     def find_point(self):
         self.point = station_geo.loc[station_geo.name == self.long_name,'geometry'].values[0]
 
+    def set_tract(self,tract):
+        self.tract = tract
+
+    def set_borough(self, borough):
+        self.borough_name = borough
+        self.borough_fips = NYC_COUNTY_FIPS[borough]
+
+    def plot_station(self):
+        point_df = gpd.GeoDataFrame({'name':'point','geometry':[self.point]},index=[0])
+        fig, ax = plt.subplots()
+        ct_df.plot(ax=ax)
+        point_df.plot(ax=ax,color='red',markersize=40)
+
 # Import station geography
 station_geo = gpd.read_file('.\\data\\shape_files\\mta_stations\\geo_export_421f3107-0ff0-4c6c-9b11-15319fb58431.shp')
 
-# Import Census tract geography
-# Import each borough separately...
-ct_shp_filenames = glob.glob('.\\data\\shape_files\\census_tracts\\*.shp')
+######################################
+# Import Census tract geography      #
+# Import each borough separately...  #
+######################################
+ct_shp_filenames = glob.glob('.\\data\\shape_files\\census_tracts_nyc\\*.shp')
 ct_dfs = []
 for filename in ct_shp_filenames:
     ct_dfs += [gpd.read_file(filename)]
 # ...and vertically concatenate
 ct_df = pd.concat(ct_dfs)
 
-
 # Import abbreviated station names
 with open('.\\data\\station_names_redux.txt','r') as f:
     station_names_redux = f.read().split('\n')
 
-
+################################################################################
+# Create dictionary mapping station names I created to shapefile station names #
+################################################################################
 station_to_shp_name_dict = {}
 for station in station_names_redux:
     ratio_list = [fuzz.ratio(station,station_2) for station_2 in np.unique(station_geo.name)]
@@ -66,31 +89,47 @@ for station in station_names_redux:
         station_to_shp_name_dict[station] = np.unique(station_geo.name)[idx][0]
 # Take broadway_jct out of dict because it's not a proper matching
 station_to_shp_name_dict.pop('broadway_jct', None)
-station_1 = Station(list(station_to_shp_name_dict.keys())[0])
 
-bool_cond =  [station_1.point.within(ct) for ct in ct_df.geometry.values]
-np.where(bool_cond)
+######################################
+# Initialize list of station objects #
+######################################
+station_objs = []
+for station_name in list(station_to_shp_name_dict.keys()):
+    # Instantiate station object
+    station = Station(station_name)
+    # Find census tract which corresponds to station
+    bool_cond =  [station.point.within(ct) for ct in ct_df.geometry.values]
+    tract = ct_df.loc[bool_cond,'CT2010'].values[0]
+    borough = ct_df.loc[bool_cond,'BoroName'].values[0]
+    station.set_tract(tract)
+    station.set_borough(borough)
+    # Add station object to list
+    station_objs += [station]
 
-point_df = gpd.GeoDataFrame({'name':'point','geometry':[station_1.point]},index=[0])
+
+#####################################
+# Plot which tracts are represented #
+#####################################
+station_points = [station.point for station in station_objs]
 fig, ax = plt.subplots()
-ct_df.plot(ax=ax)
-point_df.plot(ax=ax,color='red',markersize=40)
+for station in station_objs:
+    point_df = gpd.GeoDataFrame({'name':'point','geometry':[station.point]},index=[0])
+    point_df.plot(ax=ax,color='red',markersize=40)
+
+ct_df['include'] = ct_df.geometry.apply(lambda x: np.any([x.contains(point) for point in station_points]))
+fig, ax = plt.subplots(figsize=(12,12))
+ct_df.plot(ax=ax,column='include')
+plt.axis('off')
+plt.savefig('output\\nyc_tract_coverage.png')
+
+#########################################
+# Pull demographic info for each tract #
+#########################################
+geo_list = [censusdata.censusgeo([('state', '36'), ('county', station.borough_fips),('tract',station.tract)]) for station in station_objs]
 
 
 
-
-nyc_county_fips = {'ny_county':'061',
-                   'kings_county':'047',
-                   'queens_county':'081',
-                   'bronx_county':'005',
-                   'richmond_county':'085'}
-
-geo_list = [censusdata.censusgeo([('state', '36'), ('county', county),('tract','*')]) for county in list(nyc_county_fips.values())]
-
-manhattan = censusdata.geographies(geo_list[0],'sf1',2010)
-manhattan_censusgeo = list(manhattan.values())[:]
-
-pop_per_tract = pd.concat([censusdata.download('sf1',2010,geocode,['P008006','P001001']) for geocode in manhattan_censusgeo])
+pop_per_tract = pd.concat([censusdata.download('sf1',2010,geocode,['P008006','P001001']) for geocode in geo_list])
 
 pop_per_tract = pop_per_tract.iloc[:,[0,1]]
 pop_per_tract.columns = ['tot_asian','tot']
